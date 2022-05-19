713c3c6a894319e893b174ef7b5f330c1394d8f2
damiensedgwick <damiensedgwick@icloud.com>
2022-05-19 15:31:18 +0100

    configured basic server and basic stub tests

--- server_test.go
+++ server_test.go
@@ -1,37 +1,58 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+type StubPlayerStore struct {
+	scores map[string]int
+}
+
 func TestGETPlayers(t *testing.T) {
+	store := StubPlayerStore{
+		map[string]int{
+			"Pepper": 20,
+			"Floyd":  10,
+		},
+	}
+
+	server := &PlayerServer{&store}
+
 	t.Run("returns Pepper's score", func(t *testing.T) {
-		req, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
+		req := newGetScoreRequest("Pepper")
 		res := httptest.NewRecorder()
 
-		PlayerServer(res, req)
+		server.ServeHTTP(res, req)
 
-		got := res.Body.String()
-		want := "20"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertResponseBody(t, res.Body.String(), "20")
 	})
 
 	t.Run("returns Floyd's score", func(t *testing.T) {
-		req, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		req := newGetScoreRequest("Floyd")
 		res := httptest.NewRecorder()
 
-		PlayerServer(res, req)
+		server.ServeHTTP(res, req)
 
-		got := res.Body.String()
-		want := "10"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertResponseBody(t, res.Body.String(), "10")
 	})
+}
+
+func newGetScoreRequest(name string) *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	return req
+}
+
+func assertResponseBody(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("response body is wrong, got %q, want %q", got, want)
+	}
+}
+
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	score := s.scores[name]
+	return score
 }
