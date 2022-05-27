99161a72cd38d5c4a485c3b2a4e09d335ad14a6b
damiensedgwick <damiensedgwick@icloud.com>
2022-05-27 15:01:18 +0100

    commit point in course

--- main.go
+++ main.go
@@ -5,15 +5,7 @@ import (
 	"net/http"
 )
 
-type InMemoryPlayerStore struct{}
-
-func (i *InMemoryPlayerStore) RecordWin(name string) {}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 1234
-}
-
 func main() {
-	server := &PlayerServer{&InMemoryPlayerStore{}}
+	server := &PlayerServer{NewInMemoryPlayerStore()}
 	log.Fatal(http.ListenAndServe(":3000", server))
 }
